6866b4ba14bf6f3aeb6e171a7f541eeeef076db0
mncc8337 <mncc8327@gmail.com>
2024-06-06 23:43:44 +0700

    strategy to display notif

--- .config/awesome/ui/notification.lua
+++ .config/awesome/ui/notification.lua
@@ -3,6 +3,7 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local ruled = require("ruled")
+local gears = require("gears")
 
 naughty.config.defaults.border_width = beautiful.border_width
 
@@ -40,11 +41,38 @@ naughty.connect_signal("request::display", function(n)
 
     local img_box = nil
     if n.icon then
-        -- using the normal imagebo because naughty.widget.icon only support square image
+        -- find strategy to display icon
+        local surf = gears.surface(n.icon)
+        local width, height = gears.surface.get_size(surf)
+
+        local ideal_height = nil
+        local ideal_width = nil
+
+        -- the current width is current height is beautiful.notification_icon_size
+        local current_width = width / height * beautiful.notification_icon_size
+        local max_width     = beautiful.notification_width * 3.5/5
+
+        -- if width is too large then set fixed width
+        if current_width > max_width then
+            ideal_width = max_width
+            ideal_height = nil
+        else
+            -- set fixed height
+            ideal_width = nil
+            ideal_height = beautiful.notification_icon_size
+        end
+        -- this strategy will ensure that
+        -- the result widget is filled horizontally
+        -- while the overall layout is not overflow
+
+        -- using the normal imagebox because naughty.widget.icon only support square image
         img_box = {
             widget = wibox.widget.imagebox,
             image = n.icon,
-            forced_height = beautiful.notification_icon_size
+            forced_width = ideal_width,
+            forced_height = ideal_height,
+            align = "center",
+            valign = "center"
         }
     end
 
