d3b9f06882033cb912c27776e5ef0d7fcf8a8538
mncc8337 <mncc8327@gmail.com>
2024-06-20 15:45:44 +0700

    changed to x264 for more devices support

--- .config/awesome/config/rules.lua
+++ .config/awesome/config/rules.lua
@@ -49,7 +49,8 @@ ruled.client.connect_signal("request::rules", function()
                 "Tor Browser",
                 "Wpa_gui",
                 "veromix",
-                "xtightvncviewer"
+                "xtightvncviewer",
+                "file-roller",
             },
             -- Note that the name property shown in xprop might be set slightly after creation of the client
             -- and the name shown there might not match defined rules here.
