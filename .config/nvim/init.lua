80190818efc23c0877d32793eac3ef5dac895a8d
mncc8337 <mncc8327@gmail.com>
2024-08-01 01:13:34 +0700

    i give up on DAP thing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -88,8 +88,15 @@ lazy.setup {
             "nvim-lua/plenary.nvim"
         }
     },
-    { "mfussenegger/nvim-dap" },
-    { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+        },
+    },
 }
 
 --[[ plugins configs ]]--
@@ -117,10 +124,54 @@ require("nvim-treesitter.configs").setup {
     indent = { enable = true }
 }
 
+-- completions
+local cmp = require("cmp")
+cmp.setup {
+    snippet = {
+        expand = function(args) vim.snippet.expand(args.body) end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(
+            function(fallback)
+                if cmp.visible() then 
+                    cmp.select_next_item()
+                elseif vim.snippet.jumpable(1) then
+                    vim.snippet.jump(1)
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }
+        ),
+    },
+    sources = cmp.config.sources({{ name = "nvim_lsp" }}, {{ name = "buffer" }})
+}
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {{ name = "buffer" }}
+})
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({{ name = "path" }}, {{ name = "cmdline" }}),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- LSP
 require("mason").setup()
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason-lspconfig").setup_handlers {
     function(server_name)
-        require("lspconfig")[server_name].setup {}
+        require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+        }
     end
 }
 require("mason-lspconfig").setup()
